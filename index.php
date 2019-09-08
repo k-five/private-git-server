8c1ec6fc3dcb37c9fbc55432a07b9570869ea031
Shakiba Moshiri <hi@shakiba.it>
2019-09-08 12:36:22 +0430

    Update

--- index.php
+++ index.php
@@ -6,9 +6,10 @@
 <body id="body" class="line-numbers">
     <main>
     <?php echo file_get_contents( "src/html/logo.html" ); ?>
+    <?php echo file_get_contents( "src/html/github-gitlab-pgs.html" ); ?>
 
 
-<?php echo file_get_contents( "src/html/footer.html" ); ?>
+    <?php echo file_get_contents( "src/html/footer.html" ); ?>
     </main>
     <script type="application/javascript" src="/vendor/js/prism-5.js"></script>
 </body>
