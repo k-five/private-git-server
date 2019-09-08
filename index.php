65eb8430baa9e06d123c8c8d6405d28957584de1
Shakiba Moshiri <hi@shakiba.it>
2019-09-08 11:30:20 +0430

    Add file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php echo file_get_contents( "src/html/header.html" ) ?>
+</head>
+<body id="body" class="line-numbers">
+    <main>
+    <?php echo file_get_contents( "src/html/logo.html" ); ?>
+
+
+<?php echo file_get_contents( "src/html/footer.html" ); ?>
+    </main>
+    <script type="application/javascript" src="/vendor/js/prism-5.js"></script>
+</body>
+</html>
+
